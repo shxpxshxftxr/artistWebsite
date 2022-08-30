bc4f20f04dcd6d268469edfc1f1ac0bced3ab026
Cara Dodge <shxpxshxftxr@gmail.com>
2022-08-30 19:50:42 -0400

    added cv, contact and ig links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,35 @@
-
 import * as React from "react";
 import lizardLogo from "../images/iguanaIcon.webp";
 
 const pageStyles = {
   backgroundColor: "pink",
   color: "white",
-  padding: "19rem",
+  padding: "12rem",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
 };
 const headingStyles = {
-  maxWidth: 540,
-
+  maxWidth: 200,
+  padding: "1rem",
+  margin: "1rem",
 };
+const linkStyles = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-end",
+  margin: "1rem",
+  width: 200,
+};
+const link = {
+  color: "black",
+  fontSize: "1.25rem",
+};
+
+function clicked(e){
+  e.target.style.color = 'red'
+}
 
 const IndexPage = () => {
   return (
@@ -19,7 +37,20 @@ const IndexPage = () => {
       <h1 style={headingStyles}>
         Check back soon, website under construction!
       </h1>
-
+      <div style={linkStyles}>
+        <a
+          style={link}
+          href="https://docs.google.com/document/d/1d-HHr7kkXMagSp0ExfsjbZLj3E5VT7iBVMR2WCCKXKU/export?format=pdf"
+        >
+          cv
+        </a>
+        <a style={link} href="mailto:shxpxshxftxr@gmail.com?subject=hello">
+          electronic mail
+        </a>
+        <a style={link} href="http://www.instagram.com/dedcooter">
+          ig
+        </a>
+      </div>
     </main>
   );
 };
